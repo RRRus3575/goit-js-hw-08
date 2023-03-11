ef71da736248bd860dc467e3c1d8f7fd78640a75
Ruslan <ruslan.grudistov@gmail.com>
2023-03-11 18:43:06 +0200

    ваше сообщение

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
 
-console.log(galleryItems);
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const containerEl = document.querySelector('.gallery');
+
+const cardsMarcup = createImageEl(galleryItems);
+
+containerEl.insertAdjacentHTML('beforeend', cardsMarcup);
+
+function createImageEl(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `
+    
+    <a class="gallery__item" href ='${original}'>
+    <img class='gallery__image' src='${preview}' alt='${description}' data-source='${original}'>
+    </a>
+    
+    `;
+    })
+    .join('');
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionSelector: 'img',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+  scrollZoom: false,
+});
