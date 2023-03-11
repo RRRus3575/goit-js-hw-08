0a1c4391b540468fe2d9b5816b1e0cccea3b5a20
Ruslan <ruslan.grudistov@gmail.com>
2023-03-11 19:34:19 +0200

    commit message

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
 
+const CURRENT_TIME = 'videoplayer-current-time';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  function (data) {
+    localStorage.setItem(CURRENT_TIME, data.seconds);
+  },
+  1000
+);
+
+player.setCurrentTime(localStorage.getItem(CURRENT_TIME) || 0);
